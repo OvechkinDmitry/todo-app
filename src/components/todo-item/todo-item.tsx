77a81e2341deaadcbc05edfa40344a933152a9b3
Dmitry Ovechkin <ovechkin_dima03@mail.ru>
2023-04-27 02:36:39 +0500

    feat:added styles

--- src/components/todo-item/todo-item.tsx
+++ src/components/todo-item/todo-item.tsx
@@ -4,11 +4,14 @@ import './todo-item.css'
 export const TodoItem = () => {
     return (
         <div className={"todo-item"}>
-            <div className={"todo-item__checkbox"}>
-                <input type={'checkbox'}></input>
+            <div className={"todo-item__body"}>
+                <div className={"todo-item__checkbox"}>
+                    <input type={'checkbox'}></input>
+                </div>
+                <p className={"todo-item__info"}>Read a book</p>
+                <div>✗</div>
             </div>
-            <p className={"todo-item__info"}>Item</p>
-            <div>DELETE</div>
+            <div className={'todo-item__date'}>27.04.2003</div>
         </div>
     );
 };
--- src/components/todo/todo.tsx
+++ src/components/todo/todo.tsx
@@ -6,6 +6,7 @@ export const Todo = () => {
 
     return (
         <div className={"todo"}>
+            <h1>Add your task</h1>
             <div className={'todo__input'}>
                 <button>+</button>
                 <input type={"text"} placeholder={"add new task"}/>
